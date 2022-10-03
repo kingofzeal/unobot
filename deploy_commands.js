af3187875d4cee0f1e5bdc41fe904fdee5782e08
James McCollum <jmccollum@signet-studios.com>
2022-10-03 06:17:32 -0500

    Fix env

--- deploy_commands.js
+++ deploy_commands.js
@@ -3,10 +3,10 @@ const fs = require('node:fs');
 const path = require('node:path');
 require('dotenv').config()
 
-const APPLICATION_ID = process.env.clientId;
-const TOKEN = process.env.token;
+const APPLICATION_ID = process.env.APPLICATION_ID;
+const TOKEN = process.env.TOKEN;
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set';
-const GUILD_ID = process.env.guildId;
+const GUILD_ID = process.env.GUILD_ID;
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
